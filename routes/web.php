063e617c4ba4d0a0c5a7cc351db5b48092a93e17
Ptitlu42 <lucas.beyer@gmx.fr>
2025-05-23 21:59:44 +0200

    Ajouter une route de test pour Customers API

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/", function () {
+    return view("welcome");
+});
+
+Route::get("/api/test", function () {
+    return response()->json([
+        "status" => "success",
+        "service" => "customers-api",
+        "message" => "Le service Customers API fonctionne correctement",
+        "data" => [
+            "customers_count" => 42,
+            "version" => "1.0.0"
+        ]
+    ]);
 });
