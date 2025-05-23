3bafe29e16ab1aea3a9ae11e35a6d7320ecf873b
Ptitlu42 <lucas.beyer@gmx.fr>
2025-05-23 22:07:04 +0200

    Corriger le chemin de la route pour le test API

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ Route::get("/", function () {
     return view("welcome");
 });
 
-Route::get("/api/test", function () {
+Route::get("/customers/api/test", function () {
     return response()->json([
         "status" => "success",
         "service" => "customers-api",
